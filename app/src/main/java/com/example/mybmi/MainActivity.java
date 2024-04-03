ddb53f101e8fbde5a29f3041913bf6d7cdbc9e73
John Doe <johndoe@example.com>
2024-04-03 15:52:43 +0800

    新增返回的按鈕 RadioButton 的寫法 CheckBox 的用法

--- app/src/main/java/com/example/mybmi/MainActivity.java
+++ app/src/main/java/com/example/mybmi/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup rbSex;
     private RadioButton rbMale;
     private RadioButton rbFemale;
+    private CheckBox cbGrape;
+    private CheckBox cbApple;
+    private CheckBox cbPomelo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,40 @@ public class MainActivity extends AppCompatActivity {
                }
            }
        });
+
+       cbGrape.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               getFruits();
+           }
+       });
+       cbApple.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getFruits();
+           }
+       });
+       cbPomelo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getFruits();
+           }
+       });
+
+    }
+
+    private void getFruits() {
+        String msg = "";
+        if (cbGrape.isChecked()){
+            msg += "葡萄";
+        }
+        if (cbApple.isChecked()){
+            msg += "蘋果";
+        }
+        if (cbPomelo.isChecked()){
+            msg += "柚子";
+        }
+        show.setText("我喜歡吃" + msg);
     }
 
     public void calcBMI(View view) {
@@ -67,6 +106,9 @@ public class MainActivity extends AppCompatActivity {
        rbSex = findViewById(R.id.rbSex);
        rbMale = findViewById(R.id.rbMale);
        rbFemale = findViewById(R.id.rbFemale);
+       cbGrape = findViewById(R.id.cbGrape);
+       cbApple = findViewById(R.id.cbApple);
+       cbPomelo = findViewById(R.id.cbPomelo);
     }
 
     public void GoNext(View view) {
